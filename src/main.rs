3513874c8f7de2afc28c3dbd2ea6a6d31e119894
jnwang <jnwangcn@gmail.com>
2025-11-13 21:00:23 +0800

    Keep system error in stderr.

--- src/main.rs
+++ src/main.rs
@@ -48,12 +48,12 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
